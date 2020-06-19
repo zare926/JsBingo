31a7e8d82fbce95873a2114a8323241db7f002d7
zare926 <ilove.boc926@gmail.com>
2020-06-19 21:52:34 +0900

    リファクタリング

--- js/main.js
+++ js/main.js
@@ -14,31 +14,26 @@
     return column;
   }
 
-  const columns = [];
-  columns[0] = createColumn(0);
-  columns[1] = createColumn(1);
-  columns[2] = createColumn(2);
-  columns[3] = createColumn(3);
-  columns[4] = createColumn(4);
+  function createColumns(){
+    const columns = [];
+    for (let i = 0; i < 5; i++){
+      columns[i] = createColumn(i);
+    }
+    columns[2][2] = 'FREE';
+    return columns;
+  }
 
-  columns[2][2] = 'FREE';
-
-  const bingo = [];
-  for (let row = 0; row < 5; row++){
-    bingo[row] = [];
-    for (let col = 0; col < 5; col++){
-      bingo[row][col] = columns[col][row];
+  function renderBingo(columns){
+    for (let row = 0; row < 5; row++){
+      const tr = document.createElement('tr');
+      for (let col = 0; col < 5; col++){
+        const td = document.createElement('td');
+        td.textContent = columns[col][row];
+        tr.appendChild(td);
+      }
+      document.querySelector('tbody').appendChild(tr);
     }
   }
-  console.table(bingo);
-
-  for (let row = 0; row < 5; row++){
-    const tr = document.createElement('tr');
-    for (let col = 0; col < 5; col++){
-      const td = document.createElement('td');
-      td.textContent = bingo[row][col];
-      tr.appendChild(td);
-    }
-    document.querySelector('tbody').appendChild(tr);
-  }
+  const columns = createColumns();
+  renderBingo(columns);
 }
