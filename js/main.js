ab0614979ebc7ded3bb7f96723e049256bbf37ec
zare926 <ilove.boc926@gmail.com>
2020-06-19 21:17:12 +0900

    console.table

--- js/main.js
+++ js/main.js
@@ -7,12 +7,11 @@
       source[i] = i + 1 + 15 * col;
     }
   
-    const b = [];
+    const column = [];
     for (let i = 0; i < 5; i++){
-      b[i] = source.splice(Math.floor(Math.random()* source.length),1)[0];
+      column[i] = source.splice(Math.floor(Math.random()* source.length),1)[0];
     }
-  
-    console.log(b);
+    return column;
   }
 
   const columns = [];
@@ -21,4 +20,8 @@
   columns[2] = createColumn(2);
   columns[3] = createColumn(3);
   columns[4] = createColumn(4);
+
+  columns[2][2] = 'FREE';
+
+  console.table(columns);
 }
